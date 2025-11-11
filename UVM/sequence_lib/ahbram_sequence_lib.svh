fc5d55cde57a3866d22a12771093a8a0771af49e
WilliamZzz <zchenkb@connect.ust.hk>
2025-11-11 23:44:32 +0800

    11-Nov-Win

---
+++ UVM/sequence_lib/ahbram_sequence_lib.svh
@@ -0,0 +1,8 @@
+`ifndef AHBRAM_SEQUENCE_LIB_SVH
+`define AHBRAM_SEQUENCE_LIB_SVH
+
+`include "ahbram_sequence_lib.svh"
+`include "ahbram_base_virtual_sequence.sv"
+`include "ahbram_smoke_virtual_sequence.sv"
+
+`endif 
