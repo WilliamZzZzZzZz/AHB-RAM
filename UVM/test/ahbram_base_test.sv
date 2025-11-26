df5d024457a2491782990f676c98846e793d175e
WilliamZzz <zchenkb@connect.ust.hk>
2025-11-26 16:42:40 +0800

    26-Nov-Mac

--- UVM/test/ahbram_base_test.sv
+++ UVM/test/ahbram_base_test.sv
@@ -11,10 +11,12 @@ virtual class ahbram_base_test extends uvm_test;
 
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
+        cfg = ahbram_configuration::type_id::create("cfg");
+
         if(!uvm_config_db#(virtual ahbram_if)::get(this, "", "vif", cfg.vif))
             `uvm_fatal("GETCFG", "cannot get virtual interface from config db")
-        uvm_config_db#(ahbram_configuration)::set(this, "env", "cfg", cfg);
 
+        uvm_config_db#(ahbram_configuration)::set(this, "env", "cfg", cfg);
         env = ahbram_env::type_id::create("env", this);
     endfunction
 
