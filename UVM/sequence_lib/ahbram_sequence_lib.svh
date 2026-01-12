24bf3031e6b57689df2d408b7b9f077e0c7ff07a
WilliamZzz <zchenkb@connect.ust.hk>
2026-01-12 16:24:41 +0800

    12-Jan-Linux

--- UVM/sequence_lib/ahbram_sequence_lib.svh
+++ UVM/sequence_lib/ahbram_sequence_lib.svh
@@ -7,5 +7,6 @@
 `include "ahbram_diff_hsize_virtual_sequence.sv"
 `include "ahbram_diff_haddr_virtual_sequence.sv"
 `include "ahbram_reset_w2r_virtual_sequence.sv"
+`include "ahbram_haddr_word_unaligned_virtual_sequence.sv"
 
 `endif 
--- UVM/test/ahbram_tests.svh
+++ UVM/test/ahbram_tests.svh
@@ -6,5 +6,6 @@
 `include "ahbram_diff_hsize_test.sv"
 `include "ahbram_diff_haddr_test.sv"
 `include "ahbram_reset_w2r_test.sv"
+`include "ahbram_haddr_word_unaligned_test.sv"
 
 `endif 
