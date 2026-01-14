2a6ec0bd2e5a14cdd8118c126cecd86411aca5d1
WilliamZzz <zchenkb@connect.ust.hk>
2026-01-14 11:14:26 +0800

    14-Jan-Linux

--- UVM/sequence_lib/ahbram_haddr_word_unaligned_virtual_sequence.sv
+++ UVM/sequence_lib/ahbram_haddr_word_unaligned_virtual_sequence.sv
@@ -16,8 +16,8 @@ class ahbram_haddr_word_unaligned_virtual_sequence extends ahbram_base_virtual_s
         for(int i = 0; i < 100; i++) begin
             std::randomize(bsize) with {bsize inside {BURST_SIZE_8BIT, BURST_SIZE_16BIT, BURST_SIZE_32BIT};};
             std::randomize(addr) with {addr inside {['h1000:'h1FFF]};
-                                    bsize == BURST_SIZE_16BIT  -> (addr[0] == 1'b1);
-                                    bsize == BURST_SIZE_32BIT -> (addr[1:0] == 2'b10);
+                                    bsize == BURST_SIZE_16BIT  -> (addr[0] == 1'b0);
+                                    bsize == BURST_SIZE_32BIT -> (addr[1:0] == 2'b00);
                                     };
             std::randomize(wr_val) with {wr_val == (i << 24) + (i << 16) + (i << 8) + i;};
             data = wr_val;
